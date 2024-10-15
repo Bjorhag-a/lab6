062504f38e35c62faa9e5afe07cf7ac30afbc1ce
Alexander Björhag <bjorhagalexander@gmail.com>
2024-10-15 20:03:13 +0200

    more work on greedy

--- R/Greedy_heuristic.R
+++ R/Greedy_heuristic.R
@@ -12,29 +12,39 @@ knapsack_objects <-
 
 
 
-Greedy_heuristic <-function(x, W){
+greedy_knapsack <-function(x, W){
   
   val_per_w <- x$v / x$w
   x$val_per_w <- val_per_w
   x <- x[order(x$val_per_w, decreasing = T), ]
   
-  total_v <- c()
+  total_v <- 0
+  total_w <- 0
   n <- nrow(x)
+  elements <- c()
   
   
   
   for (i in 1:n) {
-    if (total_v < W) {
+    
+    if (sum(total_w) + x[i, 1] < W) {
+      total_v <- total_v + x[i, 2]
+      print(total_v)
+      total_w <- total_w + x[i, 1]
       
+    } else {
+      names(x)[i]
+      break
     }
     
   }
   
-  return(list(value = balbla, elements = jajaj))
+  print(total_w)
+  return(list(value = total_v))
   
 }
 
-
+greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
 
 
 
@@ -42,7 +52,21 @@ test <- knapsack_objects[1:800,]
 val_per_w <- test$v / test$w
 test$val_per_w <- val_per_w
 test <- test[order(val_per_w, decreasing = T), ]
-
+test[1, 3]
+total_v <- 0
+total_w <- 0
+W <- 3500
+for (i in 1:800) {
+  
+  total_v <- total_v + test[i, 3]
+  total_w <- total_w + test[i, 1]
+  
+  if (sum(total_w) < W) {
+    
+    print(sum(total_v))
+  }
+  
+}
 
 greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
 greedy_knapsack(x = knapsack_objects[1:1200,], W = 2000)
